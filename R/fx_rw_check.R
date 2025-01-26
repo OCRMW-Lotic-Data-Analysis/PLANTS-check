2360f121c4fe87905d8a2bc5913835ac85e59a27
ihellman <ian.hellman@gmail.com>
2025-01-26 13:24:23 -0700

    major rw_check cleaning, new check table, initial terrestrial check code

--- R/fx_rw_check.R
+++ R/fx_rw_check.R
@@ -3,68 +3,51 @@
 rw_check <- function(speciesFile, plotLocations, counties){
 
   # Load species richness data---- 
-  species_richness_rw <- read.csv(speciesFile)
+  species_richness_dat <- read.csv(speciesFile)
   
   # Filter species richness data to only include evaluation ID and species codes----
-  species_richness_rw <- species_richness_rw %>%
-    select(3,5) %>%
-    unique()
-  
-  # Clean up column names----
-  colnames(species_richness_rw) <- c("EvaluationID", "PLANTS_code")
+  species_richness_dat <- species_richness_dat %>%
+    select(SpecRichDetailEvaluationID, Select.Plant.Species) %>%
+    rename(EvaluationID = SpecRichDetailEvaluationID, PLANT_code = Select.Plant.Species) %>%
+    distinct()
   
   # Load data file with spatial data----
-  species_richness_rw_spatial <- read.csv(plotLocations)
+  species_richness_spatial <- read.csv(plotLocations)
   
   # Filter spatial data file to include Plot ID (4), Evaluation ID (5), X and Y (20 and 21)----
-  species_richness_rw_spatial <- species_richness_rw_spatial[,c(4,5,20,21)]
-  
-  # Remove duplicates
-  species_richness_rw_spatial <- unique(species_richness_rw_spatial)
+  species_richness_spatial <- species_richness_spatial %>%
+    select(PlotID,AdminState, EvaluationID, x, y) %>%
+    distinct()
   
   # Join spatial data and species richness data by evaluationID. There are many coordinates with R&W data so this just grabs the first match.
   #NOTE: currently filter to NV but that will need to be updated with the radio button we talked about to filter by state.
-  species_sum <- species_richness_rw %>%
-    left_join(species_richness_rw_spatial, join_by(EvaluationID),multiple='first') %>%
+  species_sum <- species_richness_dat %>%
+    left_join(species_richness_spatial, join_by(EvaluationID),multiple='first') %>%
     filter(AdminState == "NV")
   
   # Format spatial data
-  species_rw<- species_sum %>%
+  species <- species_sum %>%
     st_as_sf(coords = c("x", "y"), crs = 4326)
-  
-  # Drop geometry data for the table
-  species_rw_sub <- species_rw %>% st_drop_geometry() 
   
   # Load our version of the PLANTS database. Remove Sci names because there are rendering issues due to odd symbols in some of the sci names. 
   NV_PLANT_sum <- read.csv("./appData/NV_PLANT_sum.csv")
   NV_PLANT_sum <- NV_PLANT_sum %>% select(-sci_name)
-  
-  # Transform county spatial dataspeciesFile to match the site coordinates 
-  counties$geometry <- st_transform(counties$geometry, 4326)
-  
-  # List the county/state where the site is located
-  intersect <- unlist(st_intersects(species_rw$geometry, counties$geometry))
-  
-  # Clean up
-  species_rw$county <- counties[intersect,]$NAME
-  species_rw$state <- counties[intersect,]$STATE_NAME
-  colnames(species_rw) <- c("EvaluationID","PLANT_code", "state_full","geometry", "county", "state")
+
+  # Spatial join where plot points intersect county polygons
+  species <- st_join(species, 
+                 counties %>% select(NAME, STATE_NAME), 
+                 join = st_intersects) %>%
+    rename(state_full = STATE_NAME, state = AdminState, county = NAME)
   
   # Join that lists the plant code again if it is located in the county
-  species_rw$present <-  species_rw %>%
+  species$present <-  species %>%
     left_join(NV_PLANT_sum, by=c("PLANT_code", "county"), keep=TRUE) %>%
     select(PLANT_code.y) %>%
     st_drop_geometry() %>%
     unlist()
   
   # Creates a new column with True or False by comparing columns.
-  species_rw$status <- species_rw$present %in% species_rw$PLANT_code
+  species$status <- species$present %in% species$PLANT_code
   
-  return(species_rw)
-  
-  # Drop geometry for table and make a data frame.
-  #final <- species_rw %>% st_drop_geometry()
-  #data.frame(final)
-  
-  
+  return(species)
 }
--- app.R
+++ app.R
@@ -1,7 +1,10 @@
 library(bslib)
 library(shiny)
 library(dplyr)
+library(readr)
+library(purrr)
 library(DT)
+library(reactable)
 library(leaflet)
 library(sf)
 
@@ -74,17 +77,20 @@ ui <-  page_navbar(
         uiOutput('choose_site')
         ),
       # Main Page
-      dataTableOutput("siteTable")
+      #dataTableOutput("siteTable")
+      reactableOutput("checkTable")
       )
     )
-  )
+  ) # end ui
 
 # Define server ----------------------------------------------------------------
 server <- function(input, output, session) {
   
   ## Global --------------------------------------------------------------------
   # Load counties shapefile
-  counties_shp <- st_read("./appData/counties.shp", quiet = TRUE)
+  counties_shp <- st_read("./appData/counties.shp", quiet = TRUE) %>%
+    st_transform(crs = 4326)
+  
   ## Page 1 --------------------------------------------------------------------
   # Renders table on page 1
   output$contents <- renderDataTable({
@@ -146,44 +152,23 @@ server <- function(input, output, session) {
     })
 
     # Render table after accounting for filters
-    output$siteTable <- renderDataTable({unknown.false.model.data() })
-}
+    #output$siteTable <- renderDataTable({unknown.false.model.data() })
+    output$checkTable <- renderReactable({
+      req(unknown.false.model.data())
+      tableDat <- unknown.false.model.data() %>% 
+        st_drop_geometry() %>% 
+        select(!c(present, PlotID, state_full))
+      reactable(tableDat,
+                pagination = FALSE, 
+                highlight = TRUE, 
+                compact = TRUE,
+                fullWidth = FALSE,
+                groupBy = "EvaluationID",
+                columns = dynamicColWidths(tableDat))
+                })
+    
+} # end server
 
-# #TERRESTRIAL CODE
-# 
-# # Load terrestrial file
-# species_richness_terrestrial <- read_csv("species_richness_terrestrial.csv")
-# 
-# # Grab necessary data, turn into long form (e.g., separate rows), then format X, Y to be spatial
-# species_sum <-species_richness_terrestrial %>%
-#   select(c(4,5,31,32,33)) %>%
-#   separate_rows(c(3))
-# species_coord <- species_sum %>%
-#   st_as_sf(coords = c("x", "y"), crs = 4326)
-# 
-# # Load counties
-# states <- counties(c("Montana","Idaho","NV", "North Dakota", "South Dakota"), cb = TRUE)
-# 
-# # Transform county data to be similar to site data
-# states$geometry <- st_transform(states$geometry, 4326)
-# 
-# # Same as above for R&W
-# intersect <- unlist(st_intersects(species_coord$geometry, states$geometry))
-# 
-# species_coord$county <- states[intersect,]$NAME
-# species_coord$state <- states[intersect,]$STATE_NAME
-# 
-# colnames(species_coord) <- c('Plot_ID',"Plot_Key","PLANT_code", "geometry", "county", "state")
-# 
-# sample_county <- unique(species_coord$county)
-# species_coord$present <-  species_coord %>%
-#   left_join(filter(NV_PLANT_sum, county==sample_county), join_by(PLANT_code), keep=TRUE) %>%
-#   select(PLANT_code.y) %>%
-#   st_drop_geometry() %>%
-#   unlist()
-# 
-# species_coord$status <- species_coord$present %in% species_coord$PLANT_code
-# # 
 
-# Create Shiny app ----
+# Run Shiny app ----
 shinyApp(ui, server)
