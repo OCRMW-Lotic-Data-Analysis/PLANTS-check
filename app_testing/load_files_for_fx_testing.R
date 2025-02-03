41391ef7933560f1817174ba767cea25c5be53fe
ihellman <ian.hellman@gmail.com>
2025-02-03 13:25:33 -0700

    comments.

--- app_testing/load_files_for_fx_testing.R
+++ app_testing/load_files_for_fx_testing.R
@@ -12,7 +12,7 @@ library(readxl)
 # 
 # # Way to accept multiple files
 # files <- list.files("C:/Users/ianhe/OneDrive - The University of Montana/EMMA/analysis/MIM_compile/output_to_share/mim_data_2023/2023/battle_mtn_nv_mim_2023/data_analysis_battle_mtn_nv_mim_2023/", full.names = TRUE)[1]
-# 
+
 # # Terr
 # speciesFile <- "C:/Users/ianhe/OneDrive - The University of Montana/EMMA/analysis/PLANTS-check/sampleData/species_richness_terrestrial.csv"
 # counties <- counties <- st_read("C:/Users/ianhe/OneDrive - The University of Montana/EMMA/analysis/PLANTS-check/appData/counties.gpkg", quiet = TRUE) %>%
